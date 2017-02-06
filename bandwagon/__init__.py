19cae017122fb78198ef895bfa4e207ac85f904a
Zulko <valentin.zulkower@gmail.com>
2017-02-06 18:44:34 +0000

    First docs commit

--- bandwagon/__init__.py
+++
@@ -1,7 +0,0 @@
-""" bandwagon/__init__.py """
-
-# from .BandWagon import BandWagon
-from .BandsPattern import Band, BandsPattern, BandsPatternsSet
-from .ladders import LADDER_100_to_4k, custom_ladder
-from .tools import compute_digestion_bands
-from .version import __version__
